55a413577f207e07776d863821cb818cd71776c3
meu2100 <amanda8018@gmail.com>
2024-06-05 16:01:26 +0800

    implement check/uncheck todo

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 
 app.get('/todos', (req, res) => {
   return Todo.findAll({
-    attributes: ['id', 'name'],
+    attributes: ['id', 'name', 'isComplete'],
     raw: true
   })
     .then((todos) => res.render('todos', { todos }))
@@ -50,7 +50,7 @@ app.get('/todos/:id', (req, res) => {
   const id = req.params.id
 
   return Todo.findByPk(id, {
-    attributes: ['id', 'name'],
+    attributes: ['id', 'name', 'isComplete'],
     raw: true
   })
     .then((todo) => res.render('todo', { todo }))
@@ -61,17 +61,18 @@ app.get('/todos/:id/edit', (req, res) => {
   const id = req.params.id
 
   return Todo.findByPk(id, {
-    attributes: ['id', 'name'],
+    attributes: ['id', 'name', 'isComplete'],
     raw: true
   })
     .then((todo) => res.render('edit', { todo }))
 })
 
 app.put('/todos/:id', (req, res) => {
-  const body = req.body
+  const { name, isComplete } = req.body
   const id = req.params.id
 
-  return Todo.update({ name: body.name }, { where: { id } })
+
+  return Todo.update({ name, isComplete: isComplete === 'completed' }, { where: { id } })
     .then(() => res.redirect(`/todos/${id}`))
 })
 
